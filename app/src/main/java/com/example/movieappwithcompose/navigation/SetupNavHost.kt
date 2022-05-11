30f6643cf65ecea963d8492aa29c3ab992f9d194
Ynikum <alexfalatyuk98@gmail.com>
2022-05-11 16:42:02 +0300

    updated NavHost, completed all work with screen of details (navigation and correctly showing data)

--- app/src/main/java/com/example/movieappwithcompose/navigation/SetupNavHost.kt
+++ app/src/main/java/com/example/movieappwithcompose/navigation/SetupNavHost.kt
@@ -32,7 +32,7 @@ fun SetupNavHost(navController: NavHostController, viewModel: MainViewModel){
             MainScreen(navController, viewModel)
         }
         composable(Screens.Details.route + "/{Id}"){ backStackEntry ->
-            DetailsScreen(navController = navController, viewModel = viewModel, itemId = backStackEntry.arguments?.getString("Id") ?: "1")
+            DetailsScreen(viewModel = viewModel, itemId = backStackEntry.arguments?.getString("Id") ?: "1")
         }
     }
 }
